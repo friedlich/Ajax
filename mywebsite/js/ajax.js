b8d0c23d5306c13d9d52fb78951bcf5b3e10f40b
李杨 <1164166295@qq.com>
2019-11-12 20:57:40 +0800

    2019.11.12

--- mywebsite/js/ajax.js
+++ mywebsite/js/ajax.js
@@ -1,41 +1,41 @@
-﻿function myAjax(type,url,params,dataType,callback) {
-    var xhr = new XMLHttpRequest();
+function myAjax(type, url, params, dataType, callback) {
+	var xhr = new XMLHttpRequest();
 
-    if (type == "get") {
-        if (params && params != "") {
-            url = url + "?" + params;
-        }     
-    }
-    xhr.open(type,url,true);
-    if (type == "get") {
-        xhr.send(null);
-    } else if (type == "post") {
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.send(params);
-    }
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                var result = null;
-                if (dataType == "json") {
-                    result = xhr.responseText;
-                    result = JSON.parse(result);
-                } else if (dataType == "xml") {
-                    result = xhr.responseXML;
-                } else {
-                    result = xhr.responseText;
-                }
-                if (callback) {
-                    callback(result);
-                }
-            }
-        }
-    }  
+	if (type == "get") {
+		if (params && params != "") {
+			url = url + "?" + params;
+		}
+	}
+	xhr.open(type, url, true);
+	if (type == "get") {
+		xhr.send(null);
+	} else if (type == "post") {
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send(params);
+	}
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			if (xhr.status == 200) {
+				var result = null;
+				if (dataType == "json") {
+					result = xhr.responseText;
+					result = JSON.parse(result);
+				} else if (dataType == "xml") {
+					result = xhr.responseXML;
+				} else {
+					result = xhr.responseText;
+				}
+				if (callback) {
+					callback(result);
+				}
+			}
+		}
+	}
 }
 
 
 //var obj = {
-    
+
 //    url: "dsfrefr",
 //    type: "get",
 //    data: { userName:"xiaoming",age:"18"}
@@ -44,92 +44,132 @@
 //}
 
 function myAjax2(obj) {
-    var defaults = {
-        type: "get",
-        url: "#",
-        data: {},
-        dataType: "text",
-        success:function (result) { console.log(result) }
-    }
+	var defaults = {
+		type: "get",
+		url: "#",
+		data: {},
+		dataType: "text",
+		success: function(result) {
+			console.log(result)
+		}
+	}
 
-    for (var key in obj) {
-        defaults[key] = obj[key];
-    }
+	for (var key in obj) {
+		defaults[key] = obj[key];
+	}
 
-    var xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 
-    var params = "";
-    for (var attr in defaults.data) {
-        params = params + attr + "=" + defaults.data[attr] + "&";
-    }
-    if (params) {
-        params = params.substring(0, params.length - 1);
-    }
-    if (defaults.type == "get") {
-        defaults.url = defaults.url + "?" + params;
-    }
-    xhr.open(defaults.type, defaults.url, true);
-    if (defaults.type == "get") {
-        xhr.send(null);
-    } else if (defaults.type == "post") {
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.send(params);
-    }
+	var params = "";
+	for (var attr in defaults.data) {
+		params = params + attr + "=" + defaults.data[attr] + "&";
+	}
+	if (params) {
+		params = params.substring(0, params.length - 1);
+	}
+	if (defaults.type == "get") {
+		defaults.url = defaults.url + "?" + params;
+	}
+	xhr.open(defaults.type, defaults.url, true);
+	if (defaults.type == "get") {
+		xhr.send(null);
+	} else if (defaults.type == "post") {
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send(params);
+	}
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                var result = null;
-                if (defaults.dataType == "json") {
-                    result = xhr.responseText;
-                    result = JSON.parse(result);
-                }
-                else if (defaults.dataType == "xml") {
-                    result = xhr.responseXML
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			if (xhr.status == 200) {
+				var result = null;
+				if (defaults.dataType == "json") {
+					result = xhr.responseText;
+					result = JSON.parse(result);
+				} else if (defaults.dataType == "xml") {
+					result = xhr.responseXML
 
-                } else {
-                    result =xhr.responseText
-                }
-                defaults.success(result);
-            }
-        }
-    }
+				} else {
+					result = xhr.responseText
+				}
+				defaults.success(result);
+			}
+		}
+	}
 }
+
+
+// function myAjax3(obj) {
+// 	var defaults = {
+// 		type: "get",
+// 		url: "#",
+// 		data: {},
+// 		jsonp: "callback",
+// 		success: function(data) {}
+// 	}
+
+// 	for (var key in obj) {
+// 		defaults[key] = obj[key];
+// 	}
+
+// 	var params = "";
+// 	for (var attr in defaults.data) {
+// 		params = params + attr + "=" + defaults.data[attr] + "&";
+// 	}
+// 	if (params) {
+// 		params = params.substring(0, params.length - 1);
+// 		defaults.url += "?" + params;
+// 	}
+
+// 	defaults.url += "&" + defaults.jsonp + "=qqq";
+
+// 	var script = document.createElement("script");
+// 	script.src = defaults.url;
+
+// 	var head = document.getElementsByTagName("head")[0];
+// 	head.appendChild(script);
+
+// 	window["qqq"] = function(data) {
+// 		defaults.success(data)
+// 	}
+// }
+
+
+
+
 
 
 function myAjax3(obj) {
-    var defaults = {
-        type: "get",
-        url: "#",
-        data: {},
-        jsonp:"callback",
-        success: function (data) { }
-    }
+	var defaults = {
+		type: "get",
+		url: "#",
+		data: {},
+		jsonp: "callback",
+		success: function(data) {}
+	}
 
-    for (var key in obj) {
-        defaults[key] = obj[key];
-    }
+	for (var key in obj) {
+		defaults[key] = obj[key];
+	}
 
-    var params = "";
-    for (var attr in defaults.data) {
-        params = params + attr + "=" + defaults.data[attr] + "&";
-    }
-    if (params) {
-        params = params.substring(0, params.length - 1);
-        defaults.url += "?" + params;
-    }
+	var params = "";
+	for (var attr in defaults.data) {
+		params = params + attr + "=" + defaults.data[attr] + "&";
+	}
+	if (params) {
+		params = params.substring(0, params.length - 1);
+		defaults.url += "?" + params;
+	}
 
-    defaults.url += "&" + defaults.jsonp + "=qqq";
+	defaults.url += "&" + defaults.jsonp + "=qqq";
 
-    var script = document.createElement("script");
-    script.src = defaults.url;
+	var script = document.createElement("script");
+	script.src = defaults.url;
 
-    var head = document.getElementsByTagName("head")[0];
-    head.appendChild(script);
+	var head = document.getElementsByTagName("head")[0];
+	head.appendChild(script);
 
-    window["qqq"] = function (data) {
-        defaults.success(data)
-    }
+	window["qqq"] = function(data) {
+		defaults.success(data)
+	}
+
 }
-
-
